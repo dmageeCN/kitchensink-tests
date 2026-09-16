454c5ac06a136ac19405d3afe48baf4c3f20ccb3
Daniel Magee <daniel.magee@cornelisnetworks.com>
2026-09-16 15:35:24 -0400

    Fix early run_perftest.sh exit.

--- perf_test/run_perftest.sh
+++ perf_test/run_perftest.sh
@@ -4,16 +4,19 @@ THISFILE=${BASH_SOURCE[0]}
 : ${THISFILE:=$0}
 
 THISDIR=$(dirname $(realpath ${THISFILE}))
+
+set -m
+
 KITCHEN_DIR=$(dirname $THISDIR)
 kitchen_script=$KITCHEN_DIR/kitchensink_screen.sh
 ARGS='KITCHENSINK=0 OPX_NJOBS=1 END_MIN=5 PPN=16'
 
 outd=$HOME/perftest/small
 mkdir -p $outd
-$kitchen_script $ARGS SIZE=SMALL SMALL_ITER=1000 OUTDIR=$outd
+$kitchen_script $ARGS SIZE=SMALL SMALL_ITER=5000 OUTDIR=$outd
 
 outd=$HOME/perftest/large
 mkdir -p $outd
-$kitchen_script $ARGS SIZE=LARGE LARGE_ITER=10 OUTDIR=$outd
+$kitchen_script $ARGS SIZE=LARGE LARGE_ITER=30 OUTDIR=$outd
 
-$THISDIR/parse_perftest $HOME/perftest
+$THISDIR/parse_perftest.sh $HOME/perftest
